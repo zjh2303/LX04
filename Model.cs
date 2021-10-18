688a413b3e521b48efcb5166eb0e213540f7cc57
zjh2303 <61764919+zjh2303@users.noreply.github.com>
2021-10-18 19:05:14 +0800

    Add files via upload

---
+++ Model.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Model
+{
+    public class UserInfo
+    {
+        public string userName { get; set; }
+        public string passWord { get; set; }
+        public string qq { get; set; }
+        public string email { get; set; }
+        public string type { get; set; }
+        public string userImg { get; set; }
+    }
+    public class UserInfoNo : UserInfo
+    {
+        public int num { get; set; }
+    }
+    public class Page
+    {
+        public int pageIndex { get; set; }
+        public int pageSize { get; set; }
+    }
+
+    public class Activity
+    {
+        public int activityId { get; set; }
+        public int activityName { get; set; }
+        public int endTime{ get; set; }
+    }
+    public class WorkInfo
+    {
+        public int workId { get; set; }
+    
+    }
+    public class Comment
+    {
+        public int commentId { get; set; }
+
+    }
+    public class WorkFindPage:Page
+    {
+        public string workName { get; set; }
+
+    }
+    public class WorkMyPage : Page
+    {
+        public string userName { get; set; }
+
+    }
+    public class CommentPage : Page
+    {
+        public string workId { get; set; }
+
+    }
+    public class ActivityN0 : Activity
+    {
+        public int num { get; set; }
+
+    }
+    public class ActivityName
+    {
+        public int activityId { get; set; }
+        public string activityName { get; set; }
+
+    }
+    public class WorkinfoNo:WorkInfo
+    {
+        public int num { get; set; }
+        public string activityName { get; set; }
+
+    }
+    public class CommentNo : Comment
+    {
+        public int num { get; set; }
+        public string workName { get; set; }
+
+    }
+}
